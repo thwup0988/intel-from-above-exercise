50aecf6e26fd062720506401ec4bbbb0ec91c95f
samuel <ajlk82750@gmail.com>
2017-12-22 13:51:14 -0600

    FEATURE: More Function

--- js/script.js
+++ js/script.js
@@ -6,15 +6,41 @@ function changeEventHandler(event){
     if (!event.target.value) {
 	console.log("Deep Error: Catastrophe Eminem");
     }
-    else {console.log(event.target.value);
-	  console.log(coun.data);
+    else {
+	  action(coun.data,event.target.value);
 	 }
 }
 
 
 
-function action(){
-    var foo = document.getElementById("needbettername");
+function action(stuffo,stat){
+    var tab = document.createElement("table");
+    for (i = 0; i < stuffo.length; ++i){
+	if (stuffo[i].state_name === stat.toUpperCase()) {
+	    if (stuffo[i].county_name === "OTHER (COMBINED) COUNTIES") {
+	    } else {
+		console.log(stuffo[i]);
+		var trow = document.createElement("tr");
+		var tdee1 = document.createElement("td");
+		var ndee1 = document.createTextNode(stuffo[i].county_name);
+		tdee1.appendChild(ndee1);
+		trow.appendChild(tdee1);
+		var tdee2 = document.createElement("td");
+		var ndee2 = document.createTextNode(stuffo[i].Value);
+		tdee2.appendChild(ndee2);
+		trow.appendChild(tdee2);
+		var tdee3 = document.createElement("td");
+		var ndee3 = document.createElement("a");
+		ndee3.setAttribute("href","http://www.google.com");
+		var n2dee3 = document.createTextNode("Get Rainfall");
+		ndee3.appendChild(n2dee3);
+		tdee3.appendChild(ndee3);
+		trow.appendChild(tdee3);
+		tab.appendChild(trow);
+	    }
+	}
+    }
+    var element = document.getElementById("tableC");
+    element.appendChild(tab);
 
-    console.log(foo);
 }
